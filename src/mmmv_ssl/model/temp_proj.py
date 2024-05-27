1539eace6b0b3fe6dee24c553c2d936f1ba1dd28
iris.dumeur6@etu.univ-lorraine.fr <iris.dumeur6@etu.univ-lorraine.fr>
2024-05-27 16:45:16 +0200

    FIX bug padd mask common temporal projector

--- src/mmmv_ssl/model/temp_proj.py
+++ src/mmmv_ssl/model/temp_proj.py
@@ -3,7 +3,7 @@ import logging
 import numpy as np
 import torch
 import torch.nn as nn
-from einops import rearrange
+from einops import rearrange, repeat
 from torch import Tensor
 
 from mmmv_ssl.model.dataclass import OutTempProjForward
@@ -12,7 +12,6 @@ my_logger = logging.getLogger(__name__)
 
 
 class CrossAttn(nn.Module):
-
     def __init__(self, n_head, d_k, d_in):
         super().__init__()
         self.n_head = n_head
@@ -42,9 +41,9 @@ class CrossAttn(nn.Module):
         k = rearrange(k, "b t head c -> b head t c")
         my_logger.debug(f"key {k.shape}")
         if pad_mask is not None:
-            pad_mask = pad_mask[..., None, None]  # b,t,1,1
-            pad_mask = pad_mask.expand(-1, -1, n_head, q.shape[2])
-            pad_mask = rearrange(pad_mask, "b t head nq ->b head nq t")
+            pad_mask = repeat(
+                pad_mask, "b t -> b nhead nq t", nhead=n_head, nq=q.shape[2]
+            )
             my_logger.debug(f"Pad mask shape {pad_mask.shape}")
         v = torch.stack(v.split(v.shape[-1] // n_head, dim=-1))
         v = rearrange(v, "head b t c -> b head t c")
@@ -53,24 +52,25 @@ class CrossAttn(nn.Module):
         my_logger.debug(f"key {k.shape}")
         # q=q.to(v)
         output = torch.nn.functional.scaled_dot_product_attention(
-            query=q, key=k, value=v, attn_mask=pad_mask)  # B,h,nq,d_in
+            query=q, key=k, value=v, attn_mask=pad_mask
+        )  # B,h,nq,d_in
         my_logger.debug(f"output {output.shape}")
         return rearrange(output, "b h nq c -> b nq (h c)")
 
 
 class TemporalProjector(nn.Module):
-
     def __init__(self, num_heads: int, input_channels: int, n_q: int):
         super().__init__()
-        self.Q = nn.Parameter(torch.zeros(
-            (num_heads, n_q, input_channels))).requires_grad_(True)
+        self.Q = nn.Parameter(
+            torch.zeros((num_heads, n_q, input_channels))
+        ).requires_grad_(True)
         nn.init.normal_(self.Q, mean=0, std=np.sqrt(2.0 / (input_channels)))
-        self.ca_s1 = CrossAttn(n_head=num_heads,
-                               d_k=input_channels,
-                               d_in=input_channels)
-        self.ca_s2 = CrossAttn(n_head=num_heads,
-                               d_k=input_channels,
-                               d_in=input_channels)
+        self.ca_s1 = CrossAttn(
+            n_head=num_heads, d_k=input_channels, d_in=input_channels
+        )
+        self.ca_s2 = CrossAttn(
+            n_head=num_heads, d_k=input_channels, d_in=input_channels
+        )
 
     def forward(
         self,
@@ -83,15 +83,15 @@ class TemporalProjector(nn.Module):
 
         :param sits_s1: b,t1,c
         :type sits_s1:
-        :param padd_s1: b,t1
+        :param padd_s1: b,t1 means value in attention False when padded
         :type padd_s1:
         :param sits_s2: b,t2,c
         :type sits_s2:
-        :param padd_s2: b,t2
-        :type padd_s2:
+        :param padd_s2: b,t2 means value in attention False when padded
+        :type padd_s2:True
         :return:
         :rtype:
         """
-        align_s1 = self.ca_s1(v=sits_s1, q=self.Q, pad_mask=None)  # b nq dv
-        align_s2 = self.ca_s2(v=sits_s2, q=self.Q, pad_mask=None)  # b nq dv
+        align_s1 = self.ca_s1(v=sits_s1, q=self.Q, pad_mask=padd_s1)  # b nq dv
+        align_s2 = self.ca_s2(v=sits_s2, q=self.Q, pad_mask=padd_s2)  # b nq dv
         return OutTempProjForward(s1=align_s1, s2=align_s2)
