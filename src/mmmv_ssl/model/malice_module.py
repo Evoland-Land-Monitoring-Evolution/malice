f55e1775389811b9e4b54203d6f42233b5a53e20
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2024-11-14 15:07:53 +0000

    OPTIM: Half way through

--- src/mmmv_ssl/model/malice_module.py
+++ src/mmmv_ssl/model/malice_module.py
@@ -1,5 +1,6 @@
 import logging
 
+import numpy as np
 import torch
 from einops import rearrange, repeat
 from hydra.utils import instantiate
@@ -12,9 +13,12 @@ from mmmv_ssl.model.clean_ubarn_repr_encoder import CleanUBarnReprEncoder
 from mmmv_ssl.model.dataclass import OutTempProjForward
 from mmmv_ssl.model.decodeur import MetaDecoder
 from mmmv_ssl.model.encoding import PositionalEncoder
+from mmmv_ssl.model.projector import IdentityProj, ProjectorTemplate
 from mmmv_ssl.model.query_utils import TempMetaQuery
+from mmmv_ssl.module.dataclass import LatRepr, Rec, RecWithOrigin, OutMMAliseF
 
 my_logger = logging.getLogger(__name__)
+
 
 class AliseMMModule(nn.Module):
     def __init__(self, encodeur_s1, encodeur_s2, input_channels, common_temp_proj, decodeur,
@@ -22,6 +26,36 @@ class AliseMMModule(nn.Module):
                  d_repr: int = 64,
                  query_s1s2_d: int = 64,
                  pe_channels: int = 64,
+                 projector: DictConfig | ProjectorTemplate = None,
+                 ):
+        super().__init__()
+
+        self.encoder = MaliceEncoder(encodeur_s1,
+                                     encodeur_s2,
+                                     input_channels,
+                                     common_temp_proj,
+                                     d_repr,
+                                     projector)
+        self.decoder = MaliceDecoder(input_channels, decodeur,
+                                     pe_config,
+                                     d_repr,
+                                     query_s1s2_d,
+                                     pe_channels)
+
+    def forward(self, batch):
+        repr, out_emb, mm_embedding = self.encoder(batch)
+        reconstructions = self.decoder(batch, mm_embedding)
+        return OutMMAliseF(repr=repr, rec=reconstructions, emb=out_emb)
+
+
+class MaliceEncoder(nn.Module):
+    def __init__(self,
+                 encodeur_s1,
+                 encodeur_s2,
+                 input_channels,
+                 common_temp_proj,
+                 d_repr: int = 64,
+                 projector: DictConfig | ProjectorTemplate = None,
                  ):
         super().__init__()
 
@@ -47,41 +81,64 @@ class AliseMMModule(nn.Module):
 
         self.common_temp_proj = common_temp_proj
 
-        self.query_builder = TempMetaQuery(
-            pe_config=pe_config, input_channels=pe_channels
-        )
-        if isinstance(decodeur, DictConfig):
-            self.meta_decodeur = instantiate(
-                decodeur,
-                input_channels=d_repr,
-                d_q_in=(query_s1s2_d + pe_channels),
-                _recursive_=False,
-            )
-            # print(pe_channels+query_s1s2_d)
+        if isinstance(projector, DictConfig):
+            self.projector_emb = instantiate(projector, input_channels=d_repr)
+        elif (projector is None) or (self.w_inv == 0):
+            self.projector_emb = IdentityProj()
         else:
-            self.meta_decodeur: MetaDecoder = decodeur
+            self.projector_emb = projector
 
-        self.q_decod_s1 = self.define_query_decoder(query_s1s2_d)
-        self.q_decod_s2 = self.define_query_decoder(query_s1s2_d)
-
-    def forward(self, batch):
-        self.encode()
-        self.decode()
-
-    def compute_query(self, batch_sits: BatchOneMod, sat: str = "s2"):
-        """Compute query and reshape it"""
-        query = repeat(
-            self.query_builder(self.q_decod_s2 if sat == "s1" else self.q_decod_s1, batch_sits.input_doy),
-            "b t c -> b t c h w",
-            h=batch_sits.h,
-            w=batch_sits.w,
-        )
-        return rearrange(
-            query,
-            "b t (nh c )h w -> nh (b h w) t c",
-            nh=self.meta_decodeur.num_heads,
+    def compute_mm_embeddings(self, aligned_repr: OutTempProjForward, h: int, w: int):
+        embedding_s1 = rearrange(
+            aligned_repr.s1, "(view bhw) t c-> view bhw t c", view=2
         )
 
+        embedding_s2 = rearrange(
+            aligned_repr.s2, "(view bhw) t c-> view bhw t c", view=2
+        )
+        embeddings = rearrange(
+            torch.cat([embedding_s2, embedding_s1], dim=0),
+            "view bhw t c -> (view bhw) t c",
+        )
+        embeddings = self.projector_emb(embeddings)
+        embeddings = rearrange(
+            embeddings, "(view bhw) t c -> view bhw t c", view=4
+        )
+        mm_embedding = torch.cat(
+            [
+                embedding_s2[0, ...],
+                embedding_s2[0, ...],
+                embedding_s2[1, ...],
+                embedding_s2[1, ...],
+                embedding_s1[0, ...],
+                embedding_s1[0, ...],
+                embedding_s1[1, ...],
+                embedding_s1[1, ...],
+            ],
+            dim=0,
+        )  # should be s2a,s2a,s2b,s2b,s1a,s1a,s1b,s1b
+
+        rearrange_embeddings = lambda x: rearrange(
+            x, "(b h w) t c-> b t c h w", h=h, w=w
+        )
+
+        repr = LatRepr(*
+                       [rearrange_embeddings(emb) for emb in
+                        [
+                            embedding_s1[0, ...],
+                            embedding_s1[1, ...],
+                            embedding_s2[0, ...],
+                            embedding_s2[1, ...],
+                        ]
+                        ])
+        out_emb = LatRepr(
+            s2a=embeddings[0, ...],
+            s2b=embeddings[1, ...],
+            s1a=embeddings[2, ...],
+            s1b=embeddings[3, ...],
+        )
+
+        return repr, out_emb, mm_embedding
 
     def encode_views(self, batch: BatchMMSits, sat: str) -> tuple[BOutputReprEnco, torch.Tensor]:
         """Get two view of one satellite and encode them with encoder"""
@@ -106,7 +163,7 @@ class AliseMMModule(nn.Module):
             padd = rearrange(mask_tp, "b t h w -> (b h w) t ")
         return out, padd
 
-    def encode(self, batch):
+    def forward(self, batch: BatchMMSits):
         out_s1, padd_s1 = self.encode_views(batch, sat="s1")
         out_s2, padd_s2 = self.encode_views(batch, sat="s2")
 
@@ -116,6 +173,73 @@ class AliseMMModule(nn.Module):
             sits_s2=rearrange(out_s2.repr, "b t c h w -> (b h w) t c"),
             padd_s2=padd_s2,
         )
+
+        repr, out_emb, mm_embedding = self.compute_mm_embeddings(aligned_repr, batch.sits1a.h, batch.sits1a.w)
+
+        return repr, out_emb, mm_embedding
+
+
+class MaliceDecoder(nn.Module):
+    def __init__(self, input_channels, decodeur,
+                 pe_config: DictConfig | PositionalEncoder,
+                 d_repr: int = 64,
+                 query_s1s2_d: int = 64,
+                 pe_channels: int = 64,
+                 ):
+        super().__init__()
+
+        if isinstance(decodeur, DictConfig):
+            self.meta_decodeur = instantiate(
+                decodeur,
+                input_channels=d_repr,
+                d_q_in=(query_s1s2_d + pe_channels),
+                _recursive_=False,
+            )
+            # print(pe_channels+query_s1s2_d)
+        else:
+            self.meta_decodeur: MetaDecoder = decodeur
+
+        self.query_builder = TempMetaQuery(
+            pe_config=pe_config, input_channels=pe_channels
+        )
+
+        self.q_decod_s1 = self.define_query_decoder(query_s1s2_d)
+        self.q_decod_s2 = self.define_query_decoder(query_s1s2_d)
+
+        self.proj_s1 = torch.nn.Linear(
+            self.meta_decodeur.input_channels, input_channels.s1_channels
+        )
+
+        self.proj_s2 = torch.nn.Linear(
+            self.meta_decodeur.input_channels, input_channels.s2_channels
+        )
+
+    def compute_query(self, batch_sits: BatchOneMod, sat: str = "s2"):
+        """Compute query and reshape it"""
+        query = repeat(
+            self.query_builder(self.q_decod_s2 if sat == "s1" else self.q_decod_s1, batch_sits.input_doy),
+            "b t c -> b t c h w",
+            h=batch_sits.h,
+            w=batch_sits.w,
+        )
+        return rearrange(
+            query,
+            "b t (nh c )h w -> nh (b h w) t c",
+            nh=self.meta_decodeur.num_heads,
+        )
+
+    @staticmethod
+    def define_query_decoder(query):
+        q_decod = nn.Parameter(
+            torch.zeros(query)
+        ).requires_grad_(True)
+        nn.init.normal_(
+            q_decod, mean=0, std=np.sqrt(2.0 / (query))
+        )  # TODO check that
+        return q_decod
+
+    def forward(self, batch: BatchMMSits, mm_embedding: torch.Tensor) -> Rec:
+        # Decoder!
 
         query_s2a = self.compute_query(batch_sits=batch.sits2a, sat="s2")
         query_s2b = self.compute_query(batch_sits=batch.sits2b, sat="s2")
@@ -133,6 +257,60 @@ class AliseMMModule(nn.Module):
         )
         my_logger.debug(f"queries{mm_queries.shape}")
 
-        repr, out_emb, mm_embedding = self.compute_mm_embeddings(aligned_repr, batch.sits1a.h, batch.sits1a.w)
+        h, w = batch.sits2a.h, batch.sits2a.w
+        padd_mm = torch.cat(
+            [
+                repeat(batch.sits2b.padd_index, "b t -> (b h w) t ", h=h, w=w),
+                repeat(batch.sits1a.padd_index, "b t -> (b h w) t ", h=h, w=w),
+                repeat(batch.sits2a.padd_index, "b t -> (b h w) t ", h=h, w=w),
+                repeat(batch.sits1b.padd_index, "b t -> (b h w) t ", h=h, w=w),
+                repeat(batch.sits1b.padd_index, "b t -> (b h w) t ", h=h, w=w),
+                repeat(batch.sits2a.padd_index, "b t -> (b h w) t ", h=h, w=w),
+                repeat(batch.sits1a.padd_index, "b t -> (b h w) t ", h=h, w=w),
+                repeat(batch.sits2b.padd_index, "b t -> (b h w) t ", h=h, w=w),
+            ],
+            dim=0,
+        )  # 2(b h w) t
+        # print(f"padd mask {padd_mm.shape}")
 
-    def decode(self):
+        out = self.meta_decodeur.forward(
+            mm_sits=mm_embedding, padd_mm=padd_mm, mm_queries=mm_queries
+        )  # (2bhw) t d
+
+        out = rearrange(
+            out,
+            "(mod b h w) t c-> mod b t h w c",
+            mod=8,
+            b=batch.sits1a.b,
+            h=batch.sits1a.h,
+            w=batch.sits1a.w,
+        )
+        s1_rec = self.proj_s1(
+            rearrange(
+                out[[1, 3, 4, 6], ...], "mod b t c h w -> (mod b ) t c h w"
+            )
+        )  #
+        s2_rec = self.proj_s2(
+            rearrange(
+                out[[0, 2, 5, 7], ...], "mod b t c h w -> (mod b) t c h w"
+            )
+        )
+        s1_rec = rearrange(s1_rec, "(mod b) t h w c -> mod b t c h w", mod=4)
+        s2_rec = rearrange(s2_rec, "(mod b) t h w c -> mod b t c h w", mod=4)
+
+        rec = Rec(
+            s1a=RecWithOrigin(
+                same_mod=s1_rec[3, ...], other_mod=s1_rec[0, ...]
+            ),
+            s1b=RecWithOrigin(
+                same_mod=s1_rec[2, ...], other_mod=s1_rec[1, ...]
+            ),
+            s2a=RecWithOrigin(
+                same_mod=s2_rec[1, ...], other_mod=s2_rec[2, ...]
+            ),
+            s2b=RecWithOrigin(
+                same_mod=s2_rec[0, ...], other_mod=s2_rec[3, ...]
+            ),
+        )
+
+        return rec
