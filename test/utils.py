fca9fc76de861a954d87fc0c54b2448f5ac3e2e8
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2024-11-15 15:14:12 +0000

    OPTIM: Code cleaning

--- test/utils.py
+++ test/utils.py
@@ -1,29 +1,6 @@
 import torch
 
 from mmmv_ssl.data.dataclass import BatchMMSits, BatchOneMod
-from mmmv_ssl.model.config import ProjectorConfig, UTAEConfig
-from mmmv_ssl.module.instantiate import instantiate_vicregssl_module
-from mmmv_ssl.train.config import AdamConfig, CAWConfig, VicRegTrainConfig
-
-
-def default_lvicregmodule(d_in1, d_in2):
-    d_emb = 32
-    d_model = 64
-    default_proj_conf = ProjectorConfig(l_dim=[64, 64])
-    default_train_config = VicRegTrainConfig(
-        scheduler=CAWConfig(), optimizer=AdamConfig(), lr=0.001
-    )
-    return instantiate_vicregssl_module(
-        in_channels1=d_in1,
-        in_channels2=d_in2,
-        projector_local=default_proj_conf,
-        projector_bottleneck=default_proj_conf,
-        train_config=default_train_config,
-        d_emb=d_emb,
-        d_model=d_model,
-        utae=UTAEConfig(),
-        stats=None,
-    )
 
 
 def create_fake_batch(b=2, t1=10, c1=3, t2=20, c2=6, h=8, w=8) -> BatchMMSits:
