b7419c0b2543bef57c3b99e95418543da5a90fb1
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2025-03-03 10:51:10 +0000

    REFAC: Make pylint happy

--- src/mmmv_ssl/data/datamodule/mm_datamodule.py
+++ src/mmmv_ssl/data/datamodule/mm_datamodule.py
@@ -1,3 +1,7 @@
+"""
+Malice datamodule
+"""
+
 import logging
 
 import lightning.pytorch as pl
@@ -6,32 +10,30 @@ from lightning.pytorch.utilities.types import (
     EVAL_DATALOADERS,
     TRAIN_DATALOADERS,
 )
-from mt_ssl.data.transform import apply_transform_basic
 from omegaconf import DictConfig
 from openeo_mmdc.dataset.to_tensor import load_all_transforms
 from torch.utils.data import DataLoader
 
 from mmmv_ssl.data.dataclass import BatchMMSits
+from mmmv_ssl.data.datamodule.utils import apply_transform_basic
 from mmmv_ssl.data.dataset.collate_fn import collate_fn_mm_dataset
 from mmmv_ssl.data.dataset.pretraining_mm_mask import PretrainingMMMaskDataset
-from mmmv_ssl.data.dataset.pretraining_mm_year_dataset import (
-    ConfigPretrainingMMYearDataset,
-)
 
 my_logger = logging.getLogger(__name__)
 
 
 class MMMaskDataModule(pl.LightningDataModule):
+    """Malice datamodule"""
 
     def __init__(
-        self,
-        config_dataset,
-        batch_size: int = 2,
-        num_workers: int = 2,
-        path_dir_csv: None = None,
+            self,
+            config_dataset,
+            batch_size: int = 2,
+            num_workers: int = 2,
+            path_dir_csv: None = None,
     ):
         super().__init__()
-        self.train_dataset: ConfigPretrainingMMYearDataset | DictConfig = config_dataset.train
+        self.train_dataset: DictConfig = config_dataset.train
         self.val_dataset = config_dataset.val
         self.test_dataset = config_dataset.test
         self.num_workers = num_workers
@@ -39,8 +41,12 @@ class MMMaskDataModule(pl.LightningDataModule):
         self.path_dir_csv = path_dir_csv
         self.all_transform = load_all_transforms(self.path_dir_csv,
                                                  modalities=["s1_asc", "s2"])
+        self.data_val = None
+        self.data_train = None
+        self.data_test = None
 
     def setup(self, stage: str) -> None:
+        """Setup"""
         if stage == "fit":
             self.data_train: PretrainingMMMaskDataset = instantiate(
                 self.train_dataset,
@@ -91,14 +97,17 @@ class MMMaskDataModule(pl.LightningDataModule):
 
     def on_after_batch_transfer(self, batch: BatchMMSits,
                                 dataloader_idx: int) -> BatchMMSits:
-        #print("in datamodule", batch.sits2a.sits[0, 0, 0, ...])
+        """
+        Normalize on batch transfer.
+        """
+        # print("in datamodule", batch.sits2a.sits[0, 0, 0, ...])
         sits1a = apply_transform_basic(
             batch.sits1a.sits, transform=self.all_transform.s1_asc.transform)
         sits1b = apply_transform_basic(
             batch.sits1b.sits, transform=self.all_transform.s1_asc.transform)
         sits2a = apply_transform_basic(
             batch.sits2a.sits, transform=self.all_transform.s2.transform)
-        #print("in datamodule", batch.sits2a.sits[0, 0, 0, ...])
+        # print("in datamodule", batch.sits2a.sits[0, 0, 0, ...])
         sits2b = apply_transform_basic(
             batch.sits2b.sits, transform=self.all_transform.s2.transform)
 
@@ -107,11 +116,9 @@ class MMMaskDataModule(pl.LightningDataModule):
         batch.sits1a.sits = sits1a
         batch.sits1b.sits = sits1b
 
-        #print("in datamodule", batch.sits2b.sits[0, 0, 0, ...])
-
         return batch
 
     def transfer_batch_to_device(self, batch: BatchMMSits, device,
                                  dataloader_idx: int):
-
+        """To device"""
         return batch.to(device=device, dtype=None)
