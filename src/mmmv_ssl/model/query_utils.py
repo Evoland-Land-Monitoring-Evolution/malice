18ad2891f857919f40cb2c06a382b86ea9f33394
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2025-03-03 09:41:48 +0000

    OPTIM: make possible to use encoder class as an input

--- src/mmmv_ssl/model/query_utils.py
+++ src/mmmv_ssl/model/query_utils.py
@@ -1,10 +1,14 @@
+# pylint: disable=invalid-name
+
+"""
+Temporal Meta Query class for learnable queries in
+Malice decoder
+"""
+
 import torch
 from einops import repeat
 from mmmv_ssl.model.encoding import PositionalEncoder
 from torch import nn
-
-
-# from mt_ssl.model.encoding import PEConfig
 
 
 class TempMetaQuery(nn.Module):
@@ -12,21 +16,14 @@ class TempMetaQuery(nn.Module):
     Temporal Meta Query class for learnable queries in
     Malice decoder
     """
-
-    def __init__(self, pe_config: None, input_channels: int):
+    def __init__(self, input_channels: int, pe_config: PositionalEncoder | None = None):
         super().__init__()
-        # if isinstance(pe_config, DictConfig | PEConfig):
-        #     self.pe_encoding: PositionalEncoder = instantiate(
-        #         pe_config, d=input_channels
-        #     )
-        # elif isinstance(pe_config, PositionalEncoder):
-        #     self.pe_encoding: PositionalEncoder = pe_config
-        # else:
-        #     raise NotImplementedError
-
-        self.pe_encoding = PositionalEncoder(
-            d=input_channels
-        )
+        if isinstance(pe_config, PositionalEncoder):
+            self.pe_encoding: PositionalEncoder = pe_config
+        else:
+            self.pe_encoding = PositionalEncoder(
+                d=input_channels
+            )
 
     def forward(self, q: torch.Tensor, doy: torch.Tensor) -> torch.Tensor:
         """
