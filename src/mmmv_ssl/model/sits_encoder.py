f5c839b1a59ba0c345ffef048f5f4cd2f41ecd1a
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2025-03-03 09:40:07 +0000

    REFAC: make pylint happy

--- src/mmmv_ssl/model/sits_encoder.py
+++ src/mmmv_ssl/model/sits_encoder.py
@@ -1,18 +1,29 @@
+# pylint: disable=invalid-name
+"""
+Encoder class for inference.
+Takes one modality and produces its embeddings.
+"""
+
 import logging
 
 import torch
 from einops import rearrange, repeat
+from torch import nn
 
 from mmmv_ssl.data.dataclass import BatchOneMod
 from mmmv_ssl.model.clean_ubarn_repr_encoder import CleanUBarnReprEncoder
 from mmmv_ssl.model.datatypes import BOutputReprEncoder
 from mmmv_ssl.model.temp_proj import CrossAttn
-from torch import nn
 
 my_logger = logging.getLogger(__name__)
 
 
 class MonoSITSEncoder(nn.Module):
+    """
+    Encoder class for inference.
+    Takes one modality and produces its embeddings.
+    """
+
     def __init__(
             self,
             encoder: CleanUBarnReprEncoder,
@@ -26,13 +37,15 @@ class MonoSITSEncoder(nn.Module):
         self.encoder = encoder
 
     def forward(
-            self, batch_input: BatchOneMod
+            self, batch_input: BatchOneMod,
     ) -> BOutputReprEncoder:
+        """Forward pass: SSTE encoder + temporal projector"""
         batch_output: BOutputReprEncoder = self.encoder(batch_input)
-        mask_tp = repeat(~batch_input.padd_index.bool(), "b t -> b t h w", h=batch_input.h, w=batch_input.w)
+        mask_tp = repeat(~batch_input.padd_index.bool(),
+                         "b t -> b t h w", h=batch_input.h, w=batch_input.w)
         padd = rearrange(mask_tp, "b t h w -> (b h w) t ")
 
-        repr = self.temp_proj(
+        repro = self.temp_proj(
             v=rearrange(
                 batch_output.repr,
                 "B n c h w -> (B h w ) n c",
@@ -40,14 +53,20 @@ class MonoSITSEncoder(nn.Module):
             q=self.query,
             pad_mask=padd,
         )
-        my_logger.debug(f"repr shape{repr.shape}")
+        my_logger.debug(f"repr shape{repro.shape}")
         return BOutputReprEncoder(
-            repr=repr, doy=batch_output.doy
+            repr=repro, doy=batch_output.doy
         )
 
-    def reshape(self, batch_input: BatchOneMod, batch_output: BOutputReprEncoder) -> BOutputReprEncoder:
-        b, n, c, h, w = batch_input.sits.shape
-        repr = rearrange(
+    def reshape(self,
+                batch_input: BatchOneMod,
+                batch_output: BOutputReprEncoder
+                ) -> BOutputReprEncoder:
+        """
+        Reshape SITS to get spatial dims.
+        """
+        b, _, _, h, w = batch_input.sits.shape
+        repro = rearrange(
             batch_output.repr,
             "(B h w ) n c -> B n c h w",
             B=b,
@@ -56,35 +75,37 @@ class MonoSITSEncoder(nn.Module):
             w=w,
         )
         return BOutputReprEncoder(
-            repr=repr,
+            repr=repro,
             doy=batch_input.input_doy,
         )
 
     def forward_keep_input_dim(
             self, batch_input: BatchOneMod
     ) -> BOutputReprEncoder:
+        """Forward pass. Keep input dims"""
         out = self.forward(batch_input)
         return self.reshape(batch_input, out)
 
+
 class MonoSITSAuxEncoder(MonoSITSEncoder):
-    def __init__(
-            self,
-            encoder: CleanUBarnReprEncoder,
-            temp_proj_one_mod: CrossAttn,
-            query: nn.Parameter,
-    ):
-        super().__init__(encoder, temp_proj_one_mod, query)
+    """
+    Mono-modal malice encoder with auxillairy data
+    """
+
 
     def forward(
             self, batch_input: BatchOneMod,
             dem: torch.Tensor
     ) -> BOutputReprEncoder:
+        """
+        Forward pas. Encoder images and project temporally
+        """
         batch_output: BOutputReprEncoder = self.encoder(batch_input, dem)
         h, w = batch_output.repr.shape[-2:]
         mask_tp = repeat(~batch_output.padd_index.bool(), "b t -> b t h w", h=h, w=w)
         padd = rearrange(mask_tp, "b t h w -> (b h w) t ")
 
-        repr = self.temp_proj(
+        repro = self.temp_proj(
             v=rearrange(
                 batch_output.repr,
                 "B n c h w -> (B h w ) n c",
@@ -92,13 +113,14 @@ class MonoSITSAuxEncoder(MonoSITSEncoder):
             q=self.query,
             pad_mask=padd,
         )
-        my_logger.debug(f"repr shape{repr.shape}")
+        my_logger.debug(f"repr shape{repro.shape}")
         return BOutputReprEncoder(
-            repr=repr, doy=batch_output.doy
+            repr=repro, doy=batch_output.doy
         )
 
     def forward_keep_input_dim(
             self, batch_input: BatchOneMod, dem: torch.Tensor
     ) -> BOutputReprEncoder:
+        """Forward pass. Keep input dims"""
         out = self.forward(batch_input, dem)
         return self.reshape(batch_input, out)
