03972c96c20d74f0f0a4ef9ef5dc5e7aa4fc7596
Ekaterina Kalinicheva <ekaterina.kalinicheva@univ-tlse3.fr>
2025-03-03 13:50:39 +0000

    REFAC: Make pylint happy

--- src/mmmv_ssl/data/datamodule/utils.py
+++ src/mmmv_ssl/data/datamodule/utils.py
@@ -1,9 +1,15 @@
-import torch.nn as nn
+# pylint: disable=invalid-name
+"""Utils"""
+
 from einops import rearrange
 from torch import Tensor
+from torch import nn
 
 
-def apply_transform_basic(batch_sits: Tensor, transform: nn.Module) -> Tensor:
+def apply_transform_basic(batch_sits: Tensor,
+                          transform: nn.Module
+                          ) -> Tensor:
+    """Apply transform (normalization after batch transfer)"""
     b, *_ = batch_sits.shape
     batch_sits = rearrange(batch_sits, " b t c h w -> c (b t )  h w")
     batch_sits = transform(batch_sits)
